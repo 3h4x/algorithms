e60f152dd0c3f9130d13b1c1634ac3e658601299
Marcin Branski <marcin.branski@daftcode.pl>
2016-11-19 17:49:35 +0100

    Linitng tests

--- cache_lru/test_cache_lru.py
+++ cache_lru/test_cache_lru.py
@@ -20,47 +20,47 @@ class TestCacheLRU(unittest.TestCase):
 
     def test_addition_to_cache(self):
         cache_lru = CacheLRU(5)
-
         cache_lru.add(1)
+
         result = cache_lru.get()
 
         self.assertEqual(result, [1])
 
     def test_addition_same_number_of_elements_and_size(self):
         cache_lru = CacheLRU(3)
-
         cache_lru.add(1)
         cache_lru.add(2)
         cache_lru.add(3)
+
         result = cache_lru.get()
 
         self.assertEqual(result, [1, 2, 3])
 
     def test_addition_to_cache_over_size(self):
         cache_lru = CacheLRU(2)
-
         cache_lru.add(1)
         cache_lru.add(2)
         cache_lru.add(3)
+
         result = cache_lru.get()
 
         self.assertEqual(result, [2, 3])
 
     def test_get_element(self):
         cache_lru = CacheLRU(2)
-
         cache_lru.add(1)
+
         result = cache_lru.get(1)
 
         self.assertEqual(result, 1)
 
     def test_get_element_update_cache(self):
         cache_lru = CacheLRU(2)
-
         cache_lru.add(1)
         cache_lru.add(2)
         cache_lru.get(1)
         cache_lru.add(3)
+
         result = cache_lru.get()
 
         self.assertEqual(result, [1, 3])
